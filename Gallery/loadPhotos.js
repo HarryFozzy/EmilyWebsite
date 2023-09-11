018781627a430823d181ee48644bc125ae733422
HarryFozzy <119067404+HarryFozzy@users.noreply.github.com>
2023-09-11 21:41:00 +0100

    potential fix for photos not loading

--- Gallery/loadPhotos.js
+++ Gallery/loadPhotos.js
@@ -4,7 +4,7 @@ const fullPath = document.location.pathname;
 // Extract the file name from the path
 const galleryName = fullPath.split('/').pop().split('.');
 
-const folderPath = './Gallery/' + galleryName[0] + '/'; // Replace with the folder path you want to use
+const folderPath = './Gallery/' + galleryName[0]; // Replace with the folder path you want to use
 
 fetch(`/.netlify/functions/listFiles?folderPath=${encodeURIComponent(folderPath)}`)
 .then(response => response.json())
