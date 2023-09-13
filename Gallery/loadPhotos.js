c7dcf14d521ac041bba3fe152e324ea98b6a58f8
HarryFozzy <119067404+HarryFozzy@users.noreply.github.com>
2023-09-13 10:46:10 +0100

    added '/' to end of folder path

--- Gallery/loadPhotos.js
+++ Gallery/loadPhotos.js
@@ -4,7 +4,7 @@ const fullPath = document.location.pathname.toLowerCase();
 // Extract the file name from the path
 const galleryName = fullPath.split('/').pop().split('.');
 
-const folderPath = './assets/' + galleryName[0]; // Replace with the folder path you want to use
+const folderPath = './assets/' + galleryName[0] + '/'; // Replace with the folder path you want to use
 
 fetch(`/.netlify/functions/listFiles?folderPath=${encodeURIComponent(folderPath)}`)
 .then(response => response.json())
