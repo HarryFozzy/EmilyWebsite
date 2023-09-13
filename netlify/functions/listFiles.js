e3dd23120b3a40a02c40923f22209e95220efe41
HarryFozzy <119067404+HarryFozzy@users.noreply.github.com>
2023-09-13 16:35:38 +0100

    escape 2 folder levels

--- netlify/functions/listFiles.js
+++ netlify/functions/listFiles.js
@@ -4,7 +4,7 @@ const resolve = require('path');
 exports.handler = async (event, context) => {
   try {
     const folderPath = event.queryStringParameters.folderPath;
-    const directoryPath = resolve.join(__dirname, folderPath);
+    const directoryPath = resolve.join(__dirname, '../../'+folderPath);
     const files = fs.readdirSync(directoryPath);
     return {
       statusCode: 200,
