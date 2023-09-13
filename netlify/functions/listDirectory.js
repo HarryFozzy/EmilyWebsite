a8bec085e7307fa4a69d035618c5253c0964d3d9
HarryFozzy <119067404+HarryFozzy@users.noreply.github.com>
2023-09-13 11:43:35 +0100

    testing function added

---
+++ netlify/functions/listDirectory.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+exports.handler = async (event, context) => {
+  try {
+    const directoryPath = './'; // Specify the directory you want to list
+
+    // Use fs.readdirSync to list files and directories
+    const entries = fs.readdirSync(directoryPath, { withFileTypes: true });
+
+    // Process the entries and build the directory structure
+    const directoryStructure = entries.map((entry) => {
+      const isDirectory = entry.isDirectory();
+      return {
+        name: entry.name,
+        isDirectory,
+        path: isDirectory
+          ? path.join(directoryPath, entry.name)
+          : path.join(directoryPath, entry.name),
+      };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(directoryStructure),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
